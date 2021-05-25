4fd93b0634c9252d5de32540ff25902c798241e7
xbee1 <84559691+xbee1@users.noreply.github.com>
2021-05-25 12:38:50 +0530

    Add files via upload

---
+++ bot/helper/ext_utils/exceptions.py
@@ -0,0 +1,8 @@
+class DirectDownloadLinkException(Exception):
+    """Nᴏᴛ ᴍᴇᴛʜᴏᴅ ғᴏᴜɴᴅ ғᴏʀ ᴇxᴛʀᴀᴄᴛɪɴɢ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ ғʀᴏᴍ ᴛʜᴇ ʜᴛᴛᴘ ʟɪɴᴋ"""
+    pass
+
+
+class NotSupportedExtractionArchive(Exception):
+    """Tʜᴇ ᴀʀᴄʜɪᴠᴇ ғᴏʀᴍᴀᴛ ᴜꜱᴇ ɪꜱ ᴛʀʏɪɴɢ ᴛᴏ ᴇxᴛʀᴀᴄᴛ ɪꜱ ɴᴏᴛ ꜱᴜᴘᴘᴏʀᴛᴇᴅ"""
+    pass
